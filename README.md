9fdc5c0bdf19fdce628e12c747301d1f7f227669
marimuthu manoj <manojsrinivasan1999k@gmail.com>
2022-12-04 12:06:41 +0530

    initalize done
    react tailwind react-icons

--- README.md
+++
@@ -1,6 +0,0 @@
-# filter-editor
-Starting demo project for filter editor 
-package used 
-        #vite-react
-        #tailwind
-        #react-icons
---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h3 className="bg-green-700 text-white">App</h3>
+    </>
+  )
+}
+
+export default App
